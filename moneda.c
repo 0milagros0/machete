18c77dd49eaa3fce7d133edb4f5e141d457820c6
0milagros0 <109671392+0milagros0@users.noreply.github.com>
2026-03-27 20:24:37 +0100

    Add files via upload
    uso de while y frecuencia relativa

---
+++ moneda.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+	
+	int main()
+{
+	int caras = 0, secas = 0, tiro = 0;
+	float frecuenciaRelativaCaras, frecuenciaRelativaSecas;
+	
+	printf("¿Cuántas veces se lanzó la moneda? ");
+	scanf("%d", &tiro);
+	int contador = 1;
+	while (contador <= tiro)
+	{
+		int resultado;
+		printf("Ingrese el resultado del lanzamiento (0 para cara, 1 para seca): ");
+		scanf("%d", &resultado);
+		
+		if (resultado == 0)
+			caras++;
+		else
+		{
+			secas++;
+		}
+		contador++;
+	}
+	
+	frecuenciaRelativaCaras = ((float)caras / tiro) * 100;
+	frecuenciaRelativaSecas = ((float)secas / tiro) * 100;
+	
+	printf("Frecuencia relativa de caras: %.2f\n", frecuenciaRelativaCaras);
+	printf("Frecuencia relativa de secas: %.2f\n", frecuenciaRelativaSecas);
+	
+	return 0;
+}
