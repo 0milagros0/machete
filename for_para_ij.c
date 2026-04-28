f5cf2fd5a340f610e4b9f9456ca1275b2b847d2f
0milagros0 <109671392+0milagros0@users.noreply.github.com>
2026-04-28 13:24:36 -0300

    Add files via upload

---
+++ for_para_ij.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+//resultado
+//123
+//12
+//1
+int main() 
+{
+	int i, j;
+	
+	for(i=3; i>0; i--)
+	{
+		for(j=1; j<=i; j++)
+		{
+		printf("%d",j);
+		}
+	printf("\n");
+	}
+	return 0;
+}
+
