e77d583588ad496cb6f5d628b3b5e51047eae291
0milagros0 <109671392+0milagros0@users.noreply.github.com>
2026-04-28 13:29:28 -0300

    Add files via upload

---
+++ for_paramatriz.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+//resultado
+//111
+//011
+//001
+int main() 
+{
+	int i, j;
+	
+	for(i=0; i<3; i++)
+	{
+		for(j=0; j<3; j++)
+		{
+			if(j>=i){
+				printf("1");
+			}else{
+				printf("0");
+			}
+		}
+	printf("\n");
+	}
+
+	return 0;
+}
+
