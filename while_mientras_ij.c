93e42309cab4faff50278cad54cd9ab72e93f192
0milagros0 <109671392+0milagros0@users.noreply.github.com>
2026-04-28 12:57:34 -0300

    Add files via upload

---
+++ while_mientras_ij.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() 
+{
+	int i=0;
+	int j=0;
+	
+	while(i++ <3)
+	{
+		j=0;
+		while(++j<3)
+		{
+			printf("%d%d,", i, j);
+		}
+	}
+	return 0;
+}
+//resultado 11,12,21,22,31,32,
