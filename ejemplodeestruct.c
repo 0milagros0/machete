8043a859875a7544a70fa4b7835bec549e858497
0milagros0 <109671392+0milagros0@users.noreply.github.com>
2026-06-30 11:27:50 -0300

    Create ejemplodeestruct.c

---
+++ ejemplodeestruct.c
@@ -0,0 +1,92 @@
+/*
+CONSIGNA
+Indicar las estructuras que corresponden a Estructuras estáticas: Boolean, Cadena, Archivos
+Una estructura o registro es una estructura de datos que agrupa variables que pueden
+tener tipos diferentes.verdadero
+CONSIGNA
+Indicar cual o cuales de las siguientes frases corresponden a características de los
+arreglos:
+1 Un arreglo puede tener una o varias dimensiones .
+3 Para acceder a un elemento en particular de un arreglo se usa el
+nombre del arreglo seguido de un índice entre corchetes.
+CONSIGNA
+Cuáles de estas características son válidas en el algoritmo de Búsqueda Secuencial o
+Lineal?
+1 Retorna el valor buscado dentro de la estructura.
+2 Retorna el índice de la estructura en donde se encuentra el valor buscado
+3 Utiliza un ciclo iterativo y se comienza a recorrer la estructura hasta encontrar el
+valor buscado.
+CONSIGNA
+Un arreglo se declara de la siguiente manera:
+tipo nombre_arreglo[ tamaño] = {lista-valores};
+Ejemplo:
+int x[7] = {3,5,7,-2,0,1,4};
+verdadero
+CONSIGNA
+Una matriz de dos dimensiones se declara de la siguiente manera:
+int Matriz [ i ] [ j ]
+i: maneja las columnas de la matriz
+j: maneja las filas de la matriz
+falso
+CONSIGNA
+Un arreglos es finito porque hay un número especifico de elementos en el arreglo,
+CONSIGNA
+Para aplicar la búsqueda binaria en un arreglo este debe estar ordenada
+verdadero
+CONSIGNA
+El siguiente trozo de programa permite cargar caracteres en una cadena:
+while (i <pos)
+{
+printf ("Ingrese caracter:%d\n",i+1);
+scanf("%c\n",&car);
+nom[i]=car;
+i++;
+}
+verdadero
+CONSIGNA
+while(palabra[i]!='\0')
+{
+printf("\n");
+printf("%c",palabra[i]);
+i++;
+verdadero
+CONSIGNA
+Para declarar un procedimiento se debe usar el tipo de dato void.
+verdadero
+CONSIGNA
+¿Qué son los Algoritmos de Ordenamiento?
+1 Son un mecanismo por el cual se puede obtener información de forma ordenada.,
+2 Permiten ordenar los valores que han sido cargados o inicializados de forma aleatorea.
+3 Estos varian según la cantidad de comparaciones que suceden, 
+el tiempo que demoran y la cantidad de código necesario para implementarlo.
+CONSIGNA
+Una cadena se puede declarar de las siguientes maneras
+char nombre_arr[ tam ]='cadena';
+Por ejemplo, el siguiente fragmento inicializa cadena con ``hola'': char
+cadena[5]="hola"; El código anterior es equivalente a: char cadena[5]={'h','o','l','a','\0'};
+char cadena[5]; cadena[0]= ´h´; cadena[1]= ‘o‘;
+cadena[2]= ‘l‘;
+cadena[3]= ‘a‘;
+cadena[4]= ‘\0‘;
+falso
+CONSIGNA
+En un arreglo de estructura se puede aplicar el método de ordenamiento por un campo numérico.verdadero
+CONSIGNA
+
+CONSIGNA
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+char nombre[10], apellidos[10];
+printf("Introduce tu nombre: ");
+scanf("%s",&nombre);
+printf("Introduce tus apellidos: ");
+scanf("%s",&apellidos);
+printf("Usted es %s %s\n",nombre,apellidos);
+getch(); return 0;
+}
+verdadero
+
+
+*/
